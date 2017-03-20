f0013d6a502bd4c5380bb900f934dd28ed59f27e
tackelua@gmail.com <giathinhj12>
2017-03-20 11:07:48 +0700

    update core esp8266 to generic

--- dht11-thingspeak.ino
+++ dht11-thingspeak.ino
@@ -1,8 +1,8 @@
-﻿#include "DHT.h"
+﻿#include <DHT.h>
 #include <ESP8266WiFi.h>
 
 //DHT config
-#define DHTPIN D2     // what digital pin we're connected to
+#define DHTPIN 4     // what digital pin we're connected to
 //#define DHTTYPE DHT11   // DHT 11
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
